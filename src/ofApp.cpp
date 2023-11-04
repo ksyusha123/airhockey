47eb2723fbcb596a866105e18e641171d2d21425
Kseniia Astakhova <astakhova.ksyusha.07@mail.ru>
2023-11-04 15:57:58 +0500

    app is done

---
+++ src/ofApp.cpp
@@ -0,0 +1,151 @@
+#include "ofApp.h"
+#include "ofxOpenCv.h"
+
+
+void ofApp::updateScores(){
+    if (puck.pos.x == 0){
+        rightPlayerScore += 1;
+        puck.reset(cv::Point2f(initWidth / 2, initHeight / 2));
+    }
+
+    else if (puck.pos.x == initWidth - 1) {
+        leftPlayerScore += 1;
+        puck.reset(cv::Point2f(initWidth / 2, initHeight / 2));
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    ofSetWindowShape(initWidth, initHeight);
+    ofSetFrameRate(60);
+
+    vidGrabber.setVerbose(true);
+    vidGrabber.setup(initWidth, initHeight);
+    puck.reset(cv::Point2f(initWidth / 2, initHeight / 2));
+
+    font.load("Monoton-Regular.ttf", 50);
+    soundPlayer.load("hit.wav");
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    vidGrabber.update();
+
+    colorImg.setFromPixels(vidGrabber.getPixels());
+    bool newFrame = vidGrabber.isFrameNew();
+    // int sourceWidth = vidGrabber.getWidth();
+	// int sourceHeight = vidGrabber.getHeight();
+
+    int scaledWidth = initWidth / cvDownScale;
+	int scaledHeight = initHeight / cvDownScale;
+
+    if (currentImage.getWidth() != scaledWidth || currentImage.getHeight() != scaledHeight){
+        ofLogNotice() << "Allocating current image to: " << scaledWidth << " x " << scaledHeight << " | " << ofGetFrameNum() << endl;
+		currentImage.clear();
+		currentImage.allocate(scaledWidth, scaledHeight);
+		currentImage.set(0);
+		previousMat.release();
+		currentImage.getCvMat().copyTo(previousMat);
+		flowMat.release();
+		flowMat = cv::Mat(scaledHeight, scaledWidth, CV_32FC2);
+    }
+
+    if (newFrame){
+        grayImage = colorImg;
+        currentImage.scaleIntoMe(grayImage);
+        currentImage.contrastStretch();
+        currentImage.mirror(false, true);
+        if (blurAmount > 0) {
+            currentImage.blurGaussian(blurAmount);
+        }
+
+        cv::Mat currentMat = currentImage.getCvMat();
+        cv::calcOpticalFlowFarneback(previousMat,
+                                    currentMat,
+                                    flowMat,
+                                    0.5,
+                                    5,
+                                    15,
+                                    3,
+                                    7,
+                                    1.5,
+                                    cv::OPTFLOW_FARNEBACK_GAUSSIAN);
+        currentMat.copyTo(previousMat);
+
+        const cv::Point2f& flowAtPos = flowMat.at<cv::Point2f>(puck.pos.y / cvDownScale, puck.pos.x / cvDownScale);
+        bool hit = puck.move(flowAtPos, initWidth, initHeight);
+        if (hit) {
+            soundPlayer.play();
+        }
+        updateScores();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetColor( 255 );
+    colorImg.mirror(false, true);
+    colorImg.draw(0, 0);
+    ofSetColor(0, 0, 255);
+    ofDrawCircle(puck.pos.x, puck.pos.y, 40);
+    
+    ofSetColor(255, 255, 255);
+    ofDrawRectangle(glm::vec2(initWidth - 200, 0), 200, 100);
+    ofSetColor(0, 0, 255);
+    font.drawString(std::format("{}:{}", leftPlayerScore, rightPlayerScore), initWidth - 175, 75);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/ofApp.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+
+class Puck {
+    public:
+        cv::Point2f pos;
+        cv::Point2f velocity;
+
+        void reset(cv::Point2f startPos){
+            pos = startPos;
+            velocity = cv::Point2f(0, 0);
+        }
+
+        bool move(cv::Point2f flow, int sourceWidth, int sourceHeight) {
+            velocity += flow;
+            pos += velocity;
+            pos.x = ofClamp(pos.x, 0, sourceWidth - 1);
+            pos.y = ofClamp(pos.y, 0, sourceHeight - 1);
+			bool hit = false;
+
+            if (pos.y == 0 || pos.y == sourceHeight - 1){
+                velocity.y *= -1;
+				hit = true;
+            }
+
+            velocity.x += 0.05 * glm::sign(velocity.x);
+            velocity.y += 0.05 * glm::sign(velocity.y);
+
+			return hit;
+        }
+};
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+
+		void updateScores();
+		
+		ofVideoGrabber vidGrabber;
+		int initWidth = 1280;
+		int initHeight = 720; 
+		ofxCvColorImage	colorImg;
+		ofxCvGrayscaleImage grayImage;
+		ofxCvGrayscaleImage currentImage;
+		cv::Mat previousMat;
+		cv::Mat flowMat;
+		ofTrueTypeFont font;
+		ofSoundPlayer soundPlayer;
+
+		int blurAmount = 25;
+		int cvDownScale = 8;
+
+		Puck puck;
+		int leftPlayerScore;
+		int rightPlayerScore;
+};
